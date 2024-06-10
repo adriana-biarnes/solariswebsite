7abaf65c8007dc5a1f213c85721b3681af0c2257
Adriana Biarnés García <adrianabiarnesgarcia@Air-de-Adriana.lan>
2024-06-10 18:00:43 +0200

    icons display fixed

--- solaris_website/src/components/Services.jsx
+++ solaris_website/src/components/Services.jsx
@@ -1,21 +1,21 @@
 import { services } from "../constants";
+import styles from "../styles";
 
-console.log('aqui', services)
-
+console.log("aqui", services);
 
 const Services = () => {
   return (
-    <div className="bg-bgGray text-secondary rounded-3xl mt-16">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {services.map((service) => (
-          <div
-            key={service.id}
-            className="flex items-center space-x-4 p-4"
-          >
-            <span className="">{service.icon}</span>
-            <p className="text-lg">{service.content}</p>
-          </div>
-        ))}
+    <div className="bg-bgGray rounded-3xl mt-16">
+      <div>
+        <h3 className={`${styles.heading3} text-secondary`}>Our services</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-primary">
+          {services.map((service) => (
+            <div key={service.id} className="flex items-center space-x-4 p-4">
+              <img src={service.icon} className="" alt={service.id} />
+              <p className="text-lg">{service.content}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
