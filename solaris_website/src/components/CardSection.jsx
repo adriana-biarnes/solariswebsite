4acba453bb1da987d315c83d69fdb32c3cc8a734
MohamedTaha91 <Taha_Acme@hotmial.com>
2024-07-03 17:14:44 +0200

    Improve responsibity on ipad , and small screen

--- solaris_website/src/components/CardSection.jsx
+++ solaris_website/src/components/CardSection.jsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 const CardSection = () => {
   return (
     <div className="container mx-auto my-16">
-      <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 justify-stretch">
+      <div className="grid grid-cols-1 gap-1 md:grid-cols-1 lg:grid-cols-3 justify-stretch">
         {cards.map((card) => (
           <Card key={card.id} id={card.id} img={card.img} title={card.title} content={card.content} />
         ))}
--- solaris_website/src/components/Hero.jsx
+++ solaris_website/src/components/Hero.jsx
@@ -15,16 +15,16 @@ const Hero = () => {
   return (
     <div className="relative h-screen">
       <div
-        className="absolute inset-0 bg-cover bg-center lg:hidden sepia-0"
+        className="absolute inset-0 bg-cover bg-center lg:hidden md:hidden sepia-0"
         style={{ backgroundImage: `url(${mobileBackgroundImage})` }}
       />
       <div
-        className=" hidden absolute inset-0 bg-cover bg-center lg:block sepia-0"
+        className=" hidden absolute inset-0 bg-cover bg-center lg:block md:block sepia-0"
         style={{ backgroundImage: `url(${desktopBackgroundImage})` }}
       />
 
       {/* Content Hero */}
-      <div className="relative z-10 bg-black  bg-opacity-50 md:bg-inherit  flex items-center justify-center h-screen text-white sepia-0">
+      <div className="relative z-10 bg-black  bg-opacity-50 lg:bg-inherit  flex items-center justify-center h-screen text-white sepia-0">
         <div className={`${styles.paddingX} ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
             <Navbar />
@@ -33,12 +33,12 @@ const Hero = () => {
 
         {/* Rectángulo semitransparente solo en desktop */}
 
-        <div className="lg:flex lg:bg-black lg:bg-opacity-50 absolute  left-0 w-1/2 h-height lg:h-full items-center justify-center sepia-0">
+        <div className="lg:flex lg:bg-black lg:bg-opacity-50 absolute  left-0 w-1/2 h-height lg:h-full items-center justify-center">
           <div className="w-[460px]">
             <img
               src={logoSolaris}
               alt="Solaris"
-              className="my-10 pl-8 pt-8 md:w-[296px] md:h-[190px]"
+              className="my-10 pl-8 pt-8 lg:w-[296px] lg:h-[190px] sepia-0"
             />
             <div className="pr-16 sm:pr-0">
               <h1 className={`${styles.heading1} pl-8 my-8`}>
