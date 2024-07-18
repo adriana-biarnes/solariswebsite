e258a289a71a027b41760796b80065a9b735d9b1
Adriana Biarnés García <adrianabiarnesgarcia@Air-de-Adriana.lan>
2024-07-18 05:55:06 +0200

    texts added in constants/indexjs

--- solaris_website/src/constants/index.js
+++ solaris_website/src/constants/index.js
@@ -1,28 +1,30 @@
 import { expertise, partnership, trust, iconPlatform1, iconOptimisation2, iconStrategy3, iconModel4, iconDelivery5, iconOperations6, iconFrontoffice7, iconCompliance8 } from "../assets";
 
+// Navbar
 export const navLinks = [
   {
     id: "meet",
     title: "Meet Solaris",
   },
   {
+    id: "whatwedo",
+    title: "What we do",
+  },
+  {
+    id: "people",
+    title: "Our people",
+  },
+  {
+    id: "casestudies",
+    title: "Case studies",
+  },
+  {
     id: "contact",
     title: "Contact us",
   },
-  // {
-  //   id: "approach",
-  //   title: "Our approach",
-  // },
-  // {
-  //   id: "work",
-  //   title: "How we work",
-  // },
-  // {
-  //   id: "people",
-  //   title: "Our people",
-  // },
 ];
 
+// Hero 
 export const landing = [
   {
     id: "title-landing",
@@ -55,6 +57,7 @@ export const cards = [
   },
 ];
 
+// List of services landing page
 export const services = [
   {
     id: "services-1",
@@ -98,6 +101,7 @@ export const services = [
   },
 ];
 
+// General website content 
 export const textContent = [
   {
     id: "title-meetSolaris",
@@ -115,4 +119,121 @@ export const textContent = [
     id: "text2-meetSolaris",
     content: "We focus on technology, data and operating model change in the Investments industry, working with mid-to-large asset managers, asset owners and pensions funds. We support the lifecycle of a programme, from strategy, selection, design, implementation to embedding in BAU. We are proud of the team that we have built and the positive impact we have with our clients.",
   },
+  {
+    id: "title-whatWeDo",
+    content: "What we do",
+  },
+  {
+    id: "text1-whatWeDo",
+    content: "Solaris is a specialist consulting firm focused on the investments industry. We support our clients with technology, data and operating model transformation across the lifecycle of a programme, from strategy, selection, design, implementation to embedding in BAU.",
+  },
+  {
+    id: "title-ourPeople",
+    content: "What we do",
+  },
+  {
+    id: "text1-ourPeople",
+    content: "The core of Solaris is our people. We have worked on projects across the investments industry and have met some truly exceptional people, many of whom have chosen to join Solaris. We offer them the opportunity to apply themselves to complex, challenging change programmes, and to work with like-minded people who enjoy being part of our successful team.",
+  },
+  {
+    id: "text2-ourPeople",
+    content: "Solaris is a blend of permanent employees and associates, all of whom share our values of client service, integrity and focus on delivering successful outcomes for our clients. We choose our people based on their experience – you can expect that Solaris consultants will always have demonstrable direct, relevant and recent experience in their fields of expertise.",
+  },
+  {
+    id: "highlighted1-ourPeople",
+    content: "Interested in joining Solaris? We would be happy to hear from you. Contact us ",
+  },
+  {
+    id: "highlighted2-ourPeople",
+    content: "here.",
+  },
+  {
+    id: "subtitle-ourPeople",
+    content: "The leadership team",
+  },
 ];
+
+// What we do - service list
+export const whatWeDoCards =[
+  {
+    id: 'whatCard1',
+    icon: iconPlatform1,
+    title: 'Investment platform implementation',
+    content: 'Our team has worked with over 10 clients to deliver successful investment platform implementations. Our consultants have experience working with all of the major platforms, including Aladdin, Charles River, SimCorp and Bloomberg. '
+  },
+  {
+    id: 'whatCard2',
+    icon: iconOptimisation2,
+    title: 'Aladdin implementation and optimisation',
+    content: 'We believe we are the leading Aladdin consulting team in the UK, having successfully led some of the largest and most complex implementations. Our specialist team can help you get the most out of the toolset, including new modules such as ADC and Aladdin Accounting.'
+  },
+  {
+    id: 'whatCard3',
+    icon: iconStrategy3,
+    title: 'Investments data strategy, design and implementation',
+    content: 'We work with clients to design their investments data strategy, leveraging the latest toolsets. Our deep knowledge of the investments domain can optimise design and accelerate delivery. '
+  },
+  {
+    id: 'whatCard4',
+    icon: iconModel4,
+    title: 'Operating model design, selection and implementation',
+    content: 'We support clients to design their target business model leveraging technology and strategic partnerships. Our team has strong experience leading systems and outsourcing selections, and developing business cases that are board-level ready. '
+  },
+  {
+    id: 'whatCard5',
+    icon: iconDelivery5,
+    title: 'Programme delivery',
+    content: 'Our programme directors, programme leads and workstream leads have significant experience delivering large-scale change in the Investments industry. '
+  },
+  {
+    id: 'whatCard6',
+    icon: iconOperations6,
+    title: 'Front office specialists',
+    content: 'Our team of front office business analysis are specialist in the investment process and have experience across all public and private asset classes.'
+  },
+  {
+    id: 'whatCard7',
+    icon: iconFrontoffice7,
+    title: 'Operations backfill and middle office analysts',
+    content: 'We offer experienced operations backfill resources and business analysts to support the increased demands of large scale transformations, including across trade operations, derivatives, data management, cash operations, reconciliations and corporate actions.'
+  },
+  {
+    id: 'whatCard8',
+    icon: iconCompliance8,
+    title: 'Compliance rule coding',
+    content: 'We offer an end-to-end compliance rule coding service, from analysis through to testing. We have experience working with all the major platforms. '
+  }
+];
+
+export const leadershipTeam =[
+  {
+    id: 'stephen',
+    img: 123,
+    name: 'Stephen Grove',
+    role: 'Executive Director',
+    bio1: 'Stephen is a founding Executive Director of Solaris, and a consultant with over 25 years’ experience in the Investments industry. ',
+    bio2: 'His focus areas include technology and data operating model transformation, and delivering large-scale change. ',
+    bio3: 'Adept at stakeholder management, clients have asked him to take on senior and challenging roles, including interim ‘head-of’ positions and recovery of at-risk critical programmes. '
+  },
+  {
+    id: 'matthew',
+    img: 123,
+    name: 'Matthew Robinson',
+    role: 'Executive Director',
+    bio1: 'Matthew co-founded Solaris in 2014, having previously held a senior change role at Aviva Investors before moving into consulting. ',
+    bio2: 'He has recently held programme director roles, delivering significant technology, data and operating model change. ',
+    bio3: 'Clients value his extensive experience across technology, data and change, and his track record of delivering successful project outcomes. '
+  },
+  {
+    id: 'sarah',
+    img: 123,
+    name: 'Sarah Clarricoats',
+    role: 'Executive Director',
+    bio1: 'Sarah joined Solaris in June 2024, a key part of our plan to scale the business over the next few years. ',
+    bio2: 'Sarah comes from one of the big 4 consultancies and brings significant experience designing and delivering operating model change across technology, data and business design. ',
+    bio3: 'Client’s value her strategic industry insights and her breadth of experience across the value chain.  '
+  },
+]
+
+
+
