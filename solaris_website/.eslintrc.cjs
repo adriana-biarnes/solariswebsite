40a51618c8589754124c4d40c1d6b9a00b2261f9
Moe Taha <Taha_Acme@hotmail.com>
2025-11-24 14:55:05 +0100

    prettier addition and grid style change

--- solaris_website/.eslintrc.cjs
+++ solaris_website/.eslintrc.cjs
@@ -1,12 +1,13 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
+extends: [
+  'eslint:recommended',
+  'plugin:react/recommended',
+  'plugin:react/jsx-runtime',
+  'plugin:react-hooks/recommended',
+  'plugin:prettier/recommended', // 👈 add this
+],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
--- solaris_website/src/pages/OurPeople.jsx
+++ solaris_website/src/pages/OurPeople.jsx
@@ -1,61 +1,95 @@
 /* eslint-disable react/prop-types */
-import DarrenFace from "/DarrenFace.jpg"
-import DarrenBack from "/DarrenBack.jpg"
-import RichardFace from "/RichardFace.jpg"
-import RichardBack from "/RichardBack.jpg"
-import StephenFace from "/Stephen-face.png"
-import MathewFace from "/Mathew-face.png"
-import SarahFace from "/Sarah-face.png"
-import SarahBack from "/Sarah-back.png"
-import MathewBack from "/Mathew-back.png"
-import StephenBack from "/Stephen-back.png"
-import BlueNavBar from "../components/BlueNavBar";
-import BlueFooter from "../components/BlueFooter";
-import { useState } from "react";
-import { Link } from "react-router-dom"
+import DarrenFace from '/DarrenFace.jpg';
+import DarrenBack from '/DarrenBack.jpg';
+import RichardFace from '/RichardFace.jpg';
+import RichardBack from '/RichardBack.jpg';
+import StephenFace from '/Stephen-face.png';
+import MathewFace from '/Mathew-face.png';
+import SarahFace from '/Sarah-face.png';
+import SarahBack from '/Sarah-back.png';
+import MathewBack from '/Mathew-back.png';
+import StephenBack from '/Stephen-back.png';
+import BlueNavBar from '../components/BlueNavBar';
+import BlueFooter from '../components/BlueFooter';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const Card = ({face, back}) => {
-    const [isFaceVisible, setIsFaceVisible] = useState(true);
-    return(
-        <div 
-            className={`min-w-80 min-h-[420px] md:min-h-[460px] bg-transparent cursor-pointer group perspective`}
-            onClick={()=>setIsFaceVisible(!isFaceVisible)}
-        >
-            <div 
-                className={`relative preserve-3d ${!isFaceVisible ? 'rotate-y-180' : ''} w-full h-full duration-1000`}>
-                    <div className="absolute w-full h-full backface-hidden">
-                        <img src={face} alt="" className="w-full h-full rounded-3xl min-h-[460px] md:w-72 md:min-h-[420px] lg:min-h-[460px] lg:min-w-80" />
-                    </div>
-                    <div className="absolute rotate-y-180 w-full h-full backface-hidden rounded-xl">
-                        <img src={back} alt="" className="w-full h-full min-h-[480px] md:w-72 md:min-h-[420px] lg:min-h-[460px] lg:min-w-80rounded-xl" />
-                    </div>
-            </div>
-        </div>          
-    )}
-
-const OurPeople = () =>{
-    return(
-        <div className="flex flex-col min-h-screen">
-            <BlueNavBar />
-            <div className="flex flex-col px-8 py-8 md:py-26 md:px-26 gap-6 lg:mx-18 flex-grow font-opensans font-medium mb-32">
-                <div className="flex flex-col text-primary gap-10 text-justify px-2 text-[18px]">
-                    <h1 className="text-[32px] font-Lato font-semi-bold ">Our People</h1>
-                    <p>The core of Solaris is our people. We have worked on projects across the investments industry and have met some truly exceptional people, many of whom have chosen to join Solaris. We offer them the opportunity to apply themselves to complex, challenging change programmes, and to work with like-minded people who enjoy being part of our successful team.</p>
-                    <p>Solaris is a blend of permanent employees and associates, all of whom share our values of client service, integrity and focus on delivering successful outcomes for our clients. We choose our people based on their experience – you can expect that Solaris consultants will always have demonstrable direct, relevant and recent experience in their fields of expertise.</p>
-                    <p className="bg-gray-100 px-4 py-8 rounded-2xl">Interested in joining Solaris? We would be happy to hear from you. Contact us <Link to="/#contactUs" className="text-orangeWin">here</Link>.</p>
-                </div>
-                <h3 className="text-orangeWin font-bold text-xl p-6">Our Leadership Team</h3>
-                <div className="flex flex-col md:flex-row md:justify-between items-center gap-20 md:gap-2 sm:pb-6 ">
-                    <Card face={StephenFace} back={StephenBack}/>
-                    <Card face={MathewFace} back={MathewBack}/>
-                    <Card face={SarahFace} back={SarahBack}/>
-                    <Card face={DarrenFace} back={DarrenBack}/>
-                    <Card face={RichardFace} back={RichardBack}/>
-                </div>
-            </div>
-            <BlueFooter/>
+const Card = ({ face, back }) => {
+  const [isFaceVisible, setIsFaceVisible] = useState(true);
+  return (
+    <div
+      className={`min-w-80 min-h-[420px] md:min-h-[460px] bg-transparent cursor-pointer group perspective`}
+      onClick={() => setIsFaceVisible(!isFaceVisible)}
+    >
+      <div
+        className={`relative preserve-3d ${
+          !isFaceVisible ? 'rotate-y-180' : ''
+        } w-full h-full duration-1000`}
+      >
+        <div className="absolute w-full h-full backface-hidden">
+          <img
+            src={face}
+            alt=""
+            className="w-full h-full rounded-3xl min-h-[460px] md:w-72 md:min-h-[420px] lg:min-h-[460px] lg:min-w-80"
+          />
         </div>
-    )
-}
+        <div className="absolute rotate-y-180 w-full h-full backface-hidden rounded-xl">
+          <img
+            src={back}
+            alt=""
+            className="w-full h-full min-h-[480px] md:w-72 md:min-h-[420px] lg:min-h-[460px] lg:min-w-80rounded-xl"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default OurPeople
+const OurPeople = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <BlueNavBar />
+      <div className="flex flex-col px-8 py-8 md:py-26 md:px-26 gap-6 lg:mx-18 flex-grow font-opensans font-medium mb-32">
+        <div className="flex flex-col text-primary gap-10 text-justify px-2 text-[18px]">
+          <h1 className="text-[32px] font-Lato font-semi-bold ">Our People</h1>
+          <p>
+            The core of Solaris is our people. We have worked on projects across the investments
+            industry and have met some truly exceptional people, many of whom have chosen to join
+            Solaris. We offer them the opportunity to apply themselves to complex, challenging
+            change programmes, and to work with like-minded people who enjoy being part of our
+            successful team.
+          </p>
+          <p>
+            Solaris is a blend of permanent employees and associates, all of whom share our values
+            of client service, integrity and focus on delivering successful outcomes for our
+            clients. We choose our people based on their experience – you can expect that Solaris
+            consultants will always have demonstrable direct, relevant and recent experience in
+            their fields of expertise.
+          </p>
+          <p className="bg-gray-100 px-4 py-8 rounded-2xl">
+            Interested in joining Solaris? We would be happy to hear from you. Contact us{' '}
+            <Link to="/#contactUs" className="text-orangeWin">
+              here
+            </Link>
+            .
+          </p>
+        </div>
+        <h3 className="text-orangeWin font-bold text-xl p-6">Our Leadership Team</h3>
+
+        <div className="grid gap-x-10 gap-y-16 md:grid-cols-3 justify-items-center sm:pb-6">
+          {/* Row 1 */}
+          <Card face={StephenFace} back={StephenBack} className="md:col-start-1" />
+          <Card face={MathewFace} back={MathewBack} className="md:col-start-2" />
+          <Card face={SarahFace} back={SarahBack} className="md:col-start-3" />
+
+          {/* Row 2 (aligned under columns 2 & 3) */}
+          <Card face={DarrenFace} back={DarrenBack} className="md:col-start-2" />
+          <Card face={RichardFace} back={RichardBack} className="md:col-start-3" />
+        </div>
+      </div>
+      <BlueFooter />
+    </div>
+  );
+};
+
+export default OurPeople;
